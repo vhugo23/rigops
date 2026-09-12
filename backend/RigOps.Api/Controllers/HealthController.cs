6b852c2b9875f3e980f788dfd468cebf4ef1fc7f
vhugo23 <vhugodossantos23@gmail.com>
2026-09-12 17:57:51 -0500

    feat(api): add /health endpoint

---
+++ backend/RigOps.Api/Controllers/HealthController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RigOps.Api.Controllers;
+
+[ApiController]
+[Route("health")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new { status = "healthy", timestamp = DateTime.UtcNow });
+    }
+}
